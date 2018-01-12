6863c2c4598ea93147b605008d243c8118d38daf
wyd <wydlqx@163.com>
2018-01-12 16:01:51 +0800

    透明度

--- js/overlay.js
+++ js/overlay.js
@@ -7,7 +7,7 @@ var overlay = (function ($) {
         left: 0,
         width: '100%',
         height: '100%',
-        background: 'rgba(0,0,220,0.5)'
+        background: 'rgba(0,0,0,0.01)'
     });
 
     function create() {
